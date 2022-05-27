f8b8cff92ead0736a046a95c4c22ed7b501fdda6
SaiyansKing <38609240+SaiyansKing@users.noreply.github.com>
2022-05-27 20:36:51 +0200

    17.8-dev6

--- D3D11Engine/pch.h
+++ D3D11Engine/pch.h
@@ -31,7 +31,7 @@ using namespace DirectX;
 
 #define ENABLE_TESSELATION 0
 
-#define VERSION_NUMBER "17.8-dev5"
+#define VERSION_NUMBER "17.8-dev6"
 __declspec(selectany) const char* VERSION_NUMBER_STR = VERSION_NUMBER;
 
 extern bool FeatureLevel10Compatibility;

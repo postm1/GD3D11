67ec4ebbb6eec1e61794e624cdf01e0fd58c1f31
SaiyansKing <38609240+SaiyansKing@users.noreply.github.com>
2025-06-25 14:16:10 +0200

    Update version

--- D3D11Engine/pch.h
+++ D3D11Engine/pch.h
@@ -25,7 +25,7 @@
 using namespace DirectX;
 
 #ifndef VERSION_NUMBER
-#define VERSION_NUMBER "17.8-rev'SK3"
+#define VERSION_NUMBER "17.8-rev'SK4"
 #endif
 
 __declspec(selectany) const char* VERSION_NUMBER_STR = VERSION_NUMBER;
